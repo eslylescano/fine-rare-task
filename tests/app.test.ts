03684871286b8e0e7f4a2a7ee4f59c2735596fa6
Esly Lescano <eslylescano@gmail.com>
2024-04-07 17:23:28 +0200

    Create test for retrieve single product already created in mongodb

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -2,15 +2,26 @@ import request from 'supertest';
 import app from '../src/app';
 
 
-describe('GraphQL Endpoint', () => {
-  it('responds with hello world message', async () => {
+describe('GraphQL Product Query', () => {
+  it('should fetch product details correctly', async () => {
     const response = await request(app)
       .post('/graphql')
-      .send({ query: '{ hello }' })
+      .send({ query: '{ product(_id: "6612b72ff4e19865c7718e05") { name vintage _id } }' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200);
 
-    expect(response.body.data.hello).toEqual('Hello world');
+    const expectedData = {
+      data: {
+        product: {
+          name: 'Product Name',
+          vintage: '2022',
+          _id: '6612b72ff4e19865c7718e05'
+        }
+      }
+    };
+
+    expect(response.body).toEqual(expectedData);
   });
+
 });
